a85be1aae3bb53e4711b870001ee4acf2b7f2284
WonGooLee <54619721+WonGooLee@users.noreply.github.com>
2019-11-21 13:21:02 +0900

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ScriptsDao.java
@@ -0,0 +1,45 @@
+//작성자: 이원구
+// for database(1 of 3)
+// Dao (data object access) to access Notes (delete, insert..)
+
+package com.example.myapplication.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.myapplication.Script;
+
+import java.util.List;
+
+@Dao
+public interface ScriptsDao {
+
+    // if the Script exist replace it
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertScript(Script script);
+
+
+    @Delete
+    void deleteScript(Script script);
+
+
+    @Update
+    void updateScript(Script script);
+
+    // list All Notes From Database
+    @Query("SELECT * FROM scripts")
+    List<Script> getScripts();
+
+
+    @Query("SELECT * FROM scripts WHERE uniqueId = :scriptId") // get Script by uniqueId
+    Script getScriptById(int scriptId);
+
+
+    @Query("DELETE FROM scripts WHERE uniqueId = :scriptId")
+    void deleteScriptById(int scriptId);
+
+}
