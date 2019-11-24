e1b7fa4e048c518da1fb61f51831966d13058c06
tjmin <mtj2741@naver.com>
2019-11-24 21:37:18 +0900

    Connect login & signup to DB

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,16 +1,58 @@
 package com.example.myapplication;
 
-import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity  extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+//작성자 : 민태준
+public class LoginActivity extends AppCompatActivity {
+
+    private static String IP_ADDRESS = "10.0.2.2";
+    private static String TAG = "phptest";
+
+    private EditText mEditTextId;
+    private EditText mEditTextPw;
+    private ArrayList<AccountData> mArrayList;
+    private String mJsonString;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        mArrayList = new ArrayList<>();
+
+        Button b_login = (Button) findViewById(R.id.b_login);
+        b_login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                mArrayList.clear();
+
+                GetData task = new GetData();
+                task.execute( "http://" + IP_ADDRESS + "/mp/getjson.php", "");
+            }
+        });
 
         Button b_signup = (Button) findViewById(R.id.b_signup);
         b_signup.setOnClickListener(new View.OnClickListener() {
@@ -21,13 +63,166 @@ public class LoginActivity  extends Activity {
             }
         });
 
-        Button b_login = (Button) findViewById(R.id.b_login);
-        b_login.setOnClickListener(new View.OnClickListener() {
+        Button b_temp = (Button) findViewById(R.id.button_temp_login);
+        b_temp.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent2 = new Intent(getApplicationContext(),
+                Intent intent = new Intent(getApplicationContext(),
                         MainActivity.class);
-                startActivity(intent2);
+                startActivity(intent);
             }
         });
+    }
+
+
+
+    private class GetData extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(LoginActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            Log.d(TAG, "response - " + result);
+
+            if (result == null){
+            }
+            else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+            String postParameters = params[1];
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "GetData : Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+
+
+    private void showResult(){
+
+        String TAG_JSON = "webnautes";
+        String TAG_NUM = "num";
+        String TAG_ID = "id";
+        String TAG_PW ="pw";
+        String TAG_NAME ="name";
+
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String num = item.getString(TAG_NUM);
+                String id = item.getString(TAG_ID);
+                String pw = item.getString(TAG_PW);
+                String name = item.getString(TAG_NAME);
+
+                AccountData accountData = new AccountData();
+
+                accountData.setMember_num(num);
+                accountData.setMember_id(id);
+                accountData.setMember_pw(pw);
+                accountData.setMember_name(name);
+
+                mArrayList.add(accountData);
+            }
+
+            try {
+                mEditTextId = (EditText) findViewById(R.id.editText);
+                mEditTextPw = (EditText) findViewById(R.id.editText2);
+                String mId = mEditTextId.getText().toString();
+                String mPw = mEditTextPw.getText().toString();
+
+                for (AccountData p : mArrayList) {
+                    String id = p.getMember_id();
+                    String pw = p.getMember_pw();
+                    String name = p.getMember_name();
+
+                    if (id.equals(mId) && pw.equals(mPw)) {
+                        Log.d(TAG,"correct");
+                        Intent intent2 = new Intent(getApplicationContext(),
+                                MainActivity.class);
+                        startActivity(intent2);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+
+            }
+
+        } catch (JSONException e) {
+            Log.d(TAG, "showResult : ", e);
+        }
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -1,28 +1,165 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+//민태준
 public class SignUpActivity extends Activity {
+
+    private static String IP_ADDRESS = "10.0.2.2";
+    private static String TAG = "phptest";
+
+    private EditText mEditTextId;
+    private EditText mEditTextPw;
+    private EditText mEditTextName;
+    private TextView mTextViewResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
-        Button btn_done=(Button) findViewById(R.id.btn_done);
-        btn_done.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                finish();
+        mEditTextId = (EditText)findViewById(R.id.id_text);
+        mEditTextPw = (EditText)findViewById(R.id.pw_text);
+        mEditTextName = (EditText)findViewById(R.id.name_text);
+        mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
+
+        mTextViewResult.setMovementMethod(new ScrollingMovementMethod());
+
+        //signup
+        Button buttonInsert = (Button)findViewById(R.id.btn_done);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String id = mEditTextId.getText().toString();
+                String pw = mEditTextPw.getText().toString();
+                String name = mEditTextName.getText().toString();
+
+                InsertData task = new InsertData();
+                task.execute("http://" + IP_ADDRESS + "/mp/insert.php", id,pw,name);
+
+                mEditTextId.setText("");
+                mEditTextPw.setText("");
+                mEditTextName.setText("");
+
+                //finish();
             }
         });
 
+        /*
+        //cancel
         Button btn_cancel=(Button) findViewById(R.id.btn_cancel);
         btn_cancel.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 finish();
             }
         });
+
+         */
+    }
+
+
+
+    class InsertData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(SignUpActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            mTextViewResult.setText(result);
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String id = (String)params[1];
+            String pw = (String)params[2];
+            String name = (String)params[3];
+
+            String serverURL = (String)params[0];
+            String postParameters = "id=" + id + "&pw=" + pw + "&name=" + name;
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
     }
 }
